327010a471ed5d712195bcd1ac2304705b6b9274
Jakub Paplham <paplhjak@fel.cvut.cz>
2019-08-05 09:57:01 +0200

    Cleanup

--- src/my_publisher.cpp
+++ src/my_publisher.cpp
@@ -1,15 +1,8 @@
-
-//
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
-
 #include <point_cloud_transport/point_cloud_transport.h>
-
-//
 #include <rosbag/bag.h>
 #include <rosbag/view.h>
-
-int message_counter;
 
 int main(int argc, char** argv)
 {
@@ -17,31 +10,24 @@ int main(int argc, char** argv)
     ros::NodeHandle nh;
 
     point_cloud_transport::PointCloudTransport pct(nh);
-    point_cloud_transport::Publisher pub = pct.advertise("pct/point_cloud", 100);
-
-    message_counter = 0;
+    point_cloud_transport::Publisher pub = pct.advertise("pct/point_cloud", 10);
 
     rosbag::Bag bag;
-    bag.open("/home/user/Downloads/husky.bag", rosbag::bagmode::Read);
+    bag.open(argv[1], rosbag::bagmode::Read);
 
     ros::Rate loop_rate(5);
-
     for(rosbag::MessageInstance const m: rosbag::View(bag))
     {
-        if (message_counter>=1000)
-        {
-         break;
-        }
         sensor_msgs::PointCloud2::ConstPtr i = m.instantiate<sensor_msgs::PointCloud2>();
         if (i != nullptr)
         {
             pub.publish(i);
-
-            message_counter++;
-
             ros::spinOnce();
             loop_rate.sleep();
         }
+        if (!nh.ok())
+        {
+         break;
+        }
     }
-    return 0;
 }
--- src/my_subscriber.cpp
+++ src/my_subscriber.cpp
@@ -2,18 +2,13 @@
 #include <point_cloud_transport/point_cloud_transport.h>
 #include <sensor_msgs/PointCloud2.h>
 
-int message_counter;
-
 void Callback(const sensor_msgs::PointCloud2ConstPtr& msg)
 {
-    message_counter++;
-    std::cout << "Message number " << message_counter <<" received, number of points is: " << msg->width*msg->height << std::endl;
+    std::cout << "Message received, number of points is: " << msg->width*msg->height << std::endl;
 }
 
 int main(int argc, char **argv)
 {
-    message_counter = 0;
-
     ros::init(argc, argv, "point_cloud_subscriber", ros::init_options::AnonymousName);
     ros::NodeHandle nh;
 
